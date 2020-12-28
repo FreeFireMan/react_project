6bd404e2751f8b627f2c155ed83ec39a5fb03a0c
freefireman <beluh.c.s@gmail.com>
2020-12-28 17:18:47 +0200

    Updates

---
+++ precache-manifest.ebf645b0935d7986d31c241011a7922b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2f86bf81d2ddd934f19545bb4e438d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d44fb68a4e7426e538",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8411460fde150bfce6ec",
+    "url": "/static/css/main.cc9797bc.chunk.css"
+  },
+  {
+    "revision": "92d44fb68a4e7426e538",
+    "url": "/static/js/2.7c7a14ac.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/static/js/2.7c7a14ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8411460fde150bfce6ec",
+    "url": "/static/js/main.de385b15.chunk.js"
+  },
+  {
+    "revision": "77785b5d7f106e80a267",
+    "url": "/static/js/runtime-main.5bebf6b0.js"
+  }
+]);
